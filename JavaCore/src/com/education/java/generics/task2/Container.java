c8c2973bf148c6ff5988ef4487f004aba1f91782
jsedev <jse.dev@gmail.com>
2016-07-26 18:09:26 +0300

    Generics: Container is added.

---
+++ JavaCore/src/com/education/java/generics/task2/Container.java
@@ -0,0 +1,14 @@
+package com.education.java.generics.task2;
+
+public class Container<T> {
+
+	private T item;
+
+	public T getItem() {
+		return item;
+	}
+
+	public void setItem(T item) {
+		this.item = item;
+	}
+}
