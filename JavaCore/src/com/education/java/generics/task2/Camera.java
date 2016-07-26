afa7ea2d8fbb0fd18ba69b92a12fd783bf39f844
jsedev <jse.dev@gmail.com>
2016-07-26 17:52:27 +0300

    Generics: task2 is added.

---
+++ JavaCore/src/com/education/java/generics/task2/Camera.java
@@ -0,0 +1,14 @@
+package com.education.java.generics.task2;
+
+public class Camera {
+	
+	private double pixel;
+
+	public double getPixel() {
+		return pixel;
+	}
+
+	public void setPixel(double pixel) {
+		this.pixel = pixel;
+	}
+}
---
+++ JavaCore/src/com/education/java/generics/task2/Phone.java
@@ -0,0 +1,14 @@
+package com.education.java.generics.task2;
+
+public class Phone {
+	
+	private String model;
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+}
