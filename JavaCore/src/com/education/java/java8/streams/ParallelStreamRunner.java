1b85d9fbc1b244fa43e71785ace481e073b038ee
jsedev <jse.dev@gmail.com>
2016-08-01 15:46:36 +0300

    Streams: Parallel stream was implemented.

--- JavaCore/src/com/education/java/java8/streams/ParallelStreamRunner.java
+++ JavaCore/src/com/education/java/java8/streams/ParallelStreamRunner.java
@@ -4,20 +4,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class StreamRunner {
+public class ParallelStreamRunner {
 
 	public static void main(String[] args) {
 
-		StreamRunner runner = new StreamRunner();
-		runner.sumAllElements(4);
+		ParallelStreamRunner runner = new ParallelStreamRunner();
+		runner.sumAllElements(5);
 	}
 
 	private void sumAllElements(int n) {
 
 		List<Integer> list = Stream.iterate(0, i -> i + 1).limit(n).collect(Collectors.toList());
 		System.out.println("Original list: " + list);
-
-		long sum = list.stream().reduce(0, Integer::sum);
+		
+		Integer sum = list.stream().parallel().reduce(0, Integer::sum);
 		System.out.println("sum = " + sum);
 	}
 }
