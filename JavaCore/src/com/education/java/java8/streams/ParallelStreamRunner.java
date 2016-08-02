55e5cf623224ae1071aa6be9c21acda5fc008742
jsedev <jse.dev@gmail.com>
2016-08-02 12:36:13 +0300

    Streams: calculation of performance was added.

--- JavaCore/src/com/education/java/java8/streams/ParallelStreamRunner.java
+++ JavaCore/src/com/education/java/java8/streams/ParallelStreamRunner.java
@@ -2,6 +2,8 @@ package com.education.java.java8.streams;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class ParallelStreamRunner {
@@ -9,15 +11,37 @@ public class ParallelStreamRunner {
 	public static void main(String[] args) {
 
 		ParallelStreamRunner runner = new ParallelStreamRunner();
-		runner.sumAllElements(5);
+		// runner.sumAllElements(5);
+		runner.sumAllElementsIntRanged(5);
+		runner.sumAllElementsRanged(6);
 	}
 
 	private void sumAllElements(int n) {
 
 		List<Integer> list = Stream.iterate(0, i -> i + 1).limit(n).collect(Collectors.toList());
 		System.out.println("Original list: " + list);
-		
+
 		Integer sum = list.stream().parallel().reduce(0, Integer::sum);
 		System.out.println("sum = " + sum);
+	}
+
+	private void sumAllElementsIntRanged(int n) {
+
+		long start = System.nanoTime();
+
+		long sum = IntStream.rangeClosed(0, n).parallel().reduce(0, Integer::sum);
+		System.out.println("sum = " + sum);
+
+		System.out.println("sumAllElementsIntRanged(), Duration: " + (System.nanoTime() - start) / 1_000_000 + " msecs");
+	}
+
+	private void sumAllElementsRanged(int n) {
+
+		long start = System.nanoTime();
+		
+		Integer sum = Stream.iterate(0, i -> i + 1).limit(n).reduce(0, Integer::sum);
+		System.out.println("sum = " + sum);
+
+		System.out.println("sumAllElementsRanged(), Duration: " + (System.nanoTime() - start) / 1_000_000 + " msecs");
 	}
 }
