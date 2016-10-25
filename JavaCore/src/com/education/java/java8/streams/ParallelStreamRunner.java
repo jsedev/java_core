1be3ee4c6d349c7f6052c4c5a1402cc42b957fdd
jsedev <jse.dev@gmail.com>
2016-10-25 15:27:50 +0300

    Parallel stream is implemented.

--- JavaCore/src/com/education/java/java8/streams/ParallelStreamRunner.java
+++ JavaCore/src/com/education/java/java8/streams/ParallelStreamRunner.java
@@ -1,5 +1,6 @@
 package com.education.java.java8.streams;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -11,8 +12,10 @@ public class ParallelStreamRunner {
 
 		ParallelStreamRunner runner = new ParallelStreamRunner();
 		// runner.sumAllElements(5);
-		runner.sumAllElementsIntRanged(5);
-		runner.sumAllElementsRanged(6);
+		//runner.sumAllElementsIntRanged(5);
+		//runner.sumAllElementsRanged(6);
+		//runner.sumAllElementsInParallel();
+		runner.calcSquareRoots();
 	}
 
 	private void sumAllElements(int n) {
@@ -42,5 +45,40 @@ public class ParallelStreamRunner {
 		System.out.println("sum = " + sum);
 
 		System.out.println("sumAllElementsRanged(), Duration: " + (System.nanoTime() - start) / 1_000_000 + " msecs");
+	}
+	
+	private void sumAllElementsInParallel() {
+
+		int lastNum = 3;
+
+		System.out.print("Original source: ");
+		
+		IntStream.range(0, lastNum).forEach((x) -> {
+			System.out.print(x + " ");
+		});
+
+		System.out.println();
+
+		IntStream stream = IntStream.range(0, lastNum);
+
+		int sum = stream.parallel().reduce(0, (a, b) -> {
+			return a + b;
+		});
+		System.out.println("Sum: " + sum);
+	}
+	
+	private void calcSquareRoots() {
+
+		List<Double> originalSource = Arrays.asList(1.0, 4.0, 9.0);
+
+		System.out.println("Original source: " + originalSource);
+
+		double result = originalSource.parallelStream().reduce(1.0, (a, b) -> {
+			return a * Math.sqrt(b);
+		}, (a, b) -> {
+			return a * b;
+		});
+
+		System.out.println("Result: " + result);
 	}
 }
